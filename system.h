bd2e7672353e3fcc1fe6e0f29adecc213a724239
TOSHIHIKO TAMASHIRO <tama_elect@outlook.jp>
2020-04-24 21:02:11 +0900

    AM2320 libraly source code

---
+++ system.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   system.h
+ * Author: tama_
+ *
+ * Created on 2020/04/24, 18:05
+ */
+
+#ifndef SYSTEM_H
+#define	SYSTEM_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+#define _XTAL_FREQ 64000000
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "i2c.h"
+#include "AM2320.h"
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SYSTEM_H */
+
